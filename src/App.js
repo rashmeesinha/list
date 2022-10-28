5d84e259752e508a31897f3eff7e665f3845d1fb
Rashmi <rashmi.sinha1st@gmail.com>
2022-10-28 12:26:05 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [filterList, setFilterList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const dataList = await fetch("https://fakestoreapi.com/users");
+    const data = await dataList.json();
+    let userName = data.map((item) => {
+      return item.username;
+    });
+    setList(userName);
+    //console.log(list);
+  }
+  function changeHandle(e) {
+    setName(e.target.value);
+    let filteredData = list.filter((item) => {
+      return item.toLowerCase().includes(name.toLowerCase());
+    });
+    setFilterList(filteredData);
+    if(filteredData.length<=0){
+      setFilterList([]);
+    }
+    console.log(filterList, list);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={changeHandle}></input>
+      <br />
+
+      {filterList.length > 0 &&
+        filterList.map((item) => {
+          return <p>{item}</p>;
+        })}
     </div>
   );
 }
